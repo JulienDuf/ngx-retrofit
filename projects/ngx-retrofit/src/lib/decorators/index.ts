08df1f300b7893ff6efecbc807a17ac6c2d4270f
JulienDuf <dufresne_julien@hotmail.ca>
2019-03-04 16:45:07 -0500

    Edit public api

---
+++ projects/ngx-retrofit/src/lib/decorators/index.ts
@@ -0,0 +1,17 @@
+import { Delete } from './delete';
+import { Get } from './get';
+import { Patch } from './patch';
+import { Path } from './path';
+import { Post } from './post';
+import { Put } from './put';
+import { Query } from './query';
+
+export {
+    Delete,
+    Get,
+    Patch,
+    Path,
+    Post,
+    Put,
+    Query
+};
--- projects/ngx-retrofit/src/public_api.ts
+++ projects/ngx-retrofit/src/public_api.ts
@@ -27,4 +27,26 @@ export class NgxRetrofitModule {
             ]
         };
     }
+
+    public static forFeature(config: NgxRetrofitConfig, providers: object[]): ModuleWithProviders {
+        return {
+            ngModule: NgxRetrofitModule,
+            providers: [
+                NgxRetrofitBuilder,
+                {
+                    provide: NGX_RETROFIT_CONFIG,
+                    useValue: config
+                },
+                ...providers.map(x => {
+                    return {
+                        provide: x,
+                        useFactory: ngxRetrofitFactory(x as () => void),
+                        deps: [HttpClient, NgxRetrofitBuilder]
+                    };
+                })
+            ]
+        };
+    }
 }
+
+export * from './lib/decorators';
